5f89cbc1fdb8aa3a943a4b295c283262eb681d9d
Julián Palomo <julian@usemarkup.com>
2020-06-02 11:33:46 +0100

    fix(Features): refactor some staff

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
 
 const FeatureGrid = ({ gridItems }) => (
