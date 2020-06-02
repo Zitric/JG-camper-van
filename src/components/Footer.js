f3acc11bccbc7c9acd3e03be9f277c3a7254cf43
Julián Palomo <julian@usemarkup.com>
2020-06-02 11:32:12 +0100

    Remove footer's logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import logo from '../img/logo.svg';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
 import twitter from '../img/social/twitter.svg';
@@ -10,9 +9,6 @@ import vimeo from '../img/social/vimeo.svg';
 const Footer = () => {
   return (
     <footer className="footer has-background-black has-text-white-ter">
-      <div className="content has-text-centered">
-        <img src={logo} alt="Kaldi" style={{ width: '14em', height: '10em' }} />
-      </div>
       <div className="content has-text-centered has-background-black has-text-white-ter">
         <div className="container has-background-black has-text-white-ter">
           <div style={{ maxWidth: '100vw' }} className="columns">
