b4c011357dc1953c9034988c8c3e0375f746863e
Julián Palomo <julian@usemarkup.com>
2020-06-06 13:02:00 +0100

    Seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'JG Camper',
+    description: 'Alquiler de campervans y caravanas en Sevilla.',
+    url: 'https://jgcamper.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
