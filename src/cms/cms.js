938ee7e401a1eb5f3bf6565d0383703170e66c61
Julián Palomo <julian@usemarkup.com>
2020-06-17 11:25:21 +0100

    Add new cms values

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,17 +4,19 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 import CamperVansPagePreview from './preview-templates/CamperVansPagePreview';
 import FAQPagePreview from './preview-templates/FAQPagePreview';
+import PricePagePreview from './preview-templates/PricePagePreview';
+import ConditionsPagePreview from './preview-templates/ConditionPagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('camper-vans', CamperVansPagePreview);
 CMS.registerPreviewTemplate('FAQ', FAQPagePreview);
+CMS.registerPreviewTemplate('prices', PricePagePreview);
+CMS.registerPreviewTemplate('conditions', ConditionsPagePreview);
