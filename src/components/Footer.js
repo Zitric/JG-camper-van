91321a41fbd38f255bffc46ea953861afae1e001
Julián Palomo <julian@usemarkup.com>
2020-07-13 00:16:58 +0200

    Fix a couple of details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -60,32 +60,25 @@ const Footer = () => {
               </section>
             </div>
             <div className="column is-4 social">
-              <a title="facebook" href="https://facebook.com">
+              <a
+                title="facebook"
+                href="https://www.facebook.com/jgcamper"
+                target="blank"
+              >
                 <img
                   src={facebook}
                   alt="Facebook"
                   style={{ width: '1em', height: '1em' }}
                 />
               </a>
-              <a title="twitter" href="https://twitter.com">
-                <img
-                  className="fas fa-lg"
-                  src={twitter}
-                  alt="Twitter"
-                  style={{ width: '1em', height: '1em' }}
-                />
-              </a>
-              <a title="instagram" href="https://instagram.com">
+              <a
+                title="instagram"
+                href="https://www.instagram.com/jgcamper"
+                target="blank"
+              >
                 <img
                   src={instagram}
                   alt="Instagram"
-                  style={{ width: '1em', height: '1em' }}
-                />
-              </a>
-              <a title="vimeo" href="https://vimeo.com">
-                <img
-                  src={vimeo}
-                  alt="Vimeo"
                   style={{ width: '1em', height: '1em' }}
                 />
               </a>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -129,6 +129,7 @@ export const pageQuery = graphql`
           }
         }
         heroHeading
+        heroSubHeading
         heading
         carouselImages {
           image {
