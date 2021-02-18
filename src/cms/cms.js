a6210861d7f5d10c07e2ef1796b83170a261d515
Julián Palomo <julian@usemarkup.com>
2021-02-18 18:34:26 +0000

    progress

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,17 +4,21 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import IndexPagePreview from './preview-templates/IndexPagePreview';
+import ConditionPagePreview from './preview-templates/ConditionPagePreview';
 import CamperVansPagePreview from './preview-templates/CamperVansPagePreview';
 import CamperVansPostPreview from './preview-templates/CamperVansPostPreview';
 import FAQPagePreview from './preview-templates/FAQPagePreview';
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import PricePagePreview from './preview-templates/PricePagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate('index', IndexPagePreview);
-CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
-CMS.registerPreviewTemplate('camper-vans', CamperVansPagePreview);
-CMS.registerPreviewTemplate('camper-vans', CamperVansPostPreview);
-CMS.registerPreviewTemplate('FAQ', FAQPagePreview);
+CMS.registerPreviewTemplate('about-page', AboutPagePreview);
+CMS.registerPreviewTemplate('blog-post', BlogPostPreview);
+CMS.registerPreviewTemplate('condition-page', ConditionPagePreview);
+CMS.registerPreviewTemplate('camper-vans-page', CamperVansPagePreview);
+CMS.registerPreviewTemplate('camper-vans-post', CamperVansPostPreview);
+CMS.registerPreviewTemplate('FAQ-page', FAQPagePreview);
+CMS.registerPreviewTemplate('index-page', IndexPagePreview);
+CMS.registerPreviewTemplate('price-page', PricePagePreview);
